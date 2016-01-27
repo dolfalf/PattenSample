63348b64df62c5f77d26c32a01b34b0aa461ac4b
lee jaeeun <leejaeeun@sf-imac-002.local>
2016-01-27 10:48:36 +0900

    add project

---
+++ PattenSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PattenSample
+//
+//  Created by lee jaeeun on 2016/01/27.
+//  Copyright © 2016年 kjcode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PattenSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PattenSample
+//
+//  Created by lee jaeeun on 2016/01/27.
+//  Copyright © 2016年 kjcode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
